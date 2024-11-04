ca8f50d62cd56a40f8af51af038783954c99d406
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-11-04 09:17:10 +0100

    Update database schema and data for 'astronomie'; localize comments and add pricing details for stands and reservations.

--- database van toepasing/astronomie.sql
+++ database van toepasing/astronomie.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 30, 2024 at 09:57 AM
--- Server version: 9.0.1
--- PHP Version: 8.3.11
+-- Gép: 127.0.0.1:3306
+-- Létrehozás ideje: 2024. Nov 04. 08:15
+-- Kiszolgáló verziója: 9.0.1
+-- PHP verzió: 8.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,15 +18,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `astronomie`
+-- Adatbázis: `astronomie`
 --
-DROP DATABASE IF EXISTS astronomie;
-CREATE DATABASE astronomie;
-USE astronomie;
+DROP DATABASE IF EXISTS `astronomie`;
+CREATE DATABASE 'astronomie';
+USE `astronomie`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
+-- Tábla szerkezet ehhez a táblához `contact`
 --
 
 DROP TABLE IF EXISTS `contact`;
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `contact` (
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `contact`
+-- A tábla adatainak kiíratása `contact`
 --
 
 INSERT INTO `contact` (`id`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Question`) VALUES
@@ -54,7 +54,7 @@ INSERT INTO `contact` (`id`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Q
 -- --------------------------------------------------------
 
 --
--- Table structure for table `events`
+-- Tábla szerkezet ehhez a táblához `events`
 --
 
 DROP TABLE IF EXISTS `events`;
@@ -68,17 +68,17 @@ CREATE TABLE IF NOT EXISTS `events` (
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `events`
+-- A tábla adatainak kiíratása `events`
 --
 
 INSERT INTO `events` (`id`, `name`, `start_date`, `end_date`, `location`) VALUES
 (1, 'Sneakerness Van Nelle Fabriek', '2024-10-26', '2024-10-27', 'Van Nelle Fabriek'),
-(2, 'Sneakerness Millenáris Budapest', '2024-11-02', '2024-11-03', 'Millenáris Budapest');
+(2, 'Sneakerness Millenáris Budapest', '2024-12-02', '2024-12-03', 'Millenáris Budapest');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messages`
+-- Tábla szerkezet ehhez a táblához `messages`
 --
 
 DROP TABLE IF EXISTS `messages`;
@@ -90,16 +90,22 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `messages`
+-- A tábla adatainak kiíratása `messages`
 --
+
+INSERT INTO `messages` (`id`, `user_id`, `messages`, `is_read`, `created_at`) VALUES
+(12, 4, 'Je reservering is goedgekeurd.', 0, '2024-10-31 09:31:28'),
+(13, 4, 'Je reservering is goedgekeurd.', 0, '2024-10-31 09:31:29'),
+(14, 3, 'Je reservering is goedgekeurd.', 1, '2024-10-31 09:32:25'),
+(15, 4, 'Je reservering is goedgekeurd.', 0, '2024-10-31 09:54:02');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `plains`
+-- Tábla szerkezet ehhez a táblához `plains`
 --
 
 DROP TABLE IF EXISTS `plains`;
@@ -112,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `plains` (
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `plains`
+-- A tábla adatainak kiíratása `plains`
 --
 
 INSERT INTO `plains` (`id`, `event_id`, `plain_name`) VALUES
@@ -125,7 +131,7 @@ INSERT INTO `plains` (`id`, `event_id`, `plain_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservations`
+-- Tábla szerkezet ehhez a táblához `reservations`
 --
 
 DROP TABLE IF EXISTS `reservations`;
@@ -135,19 +141,27 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `stand_id` int DEFAULT NULL,
   `company_name` varchar(255) DEFAULT NULL,
   `statuses` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'active',
+  `days` int NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `stand_id` (`stand_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `reservations`
+-- A tábla adatainak kiíratása `reservations`
 --
+
+INSERT INTO `reservations` (`id`, `user_id`, `stand_id`, `company_name`, `statuses`, `days`, `total_price`) VALUES
+(13, 4, 2, 'Sneaker Design505', 'approved', 2, 300.00),
+(14, 4, 25, 'kutklotenaam', 'approved', 2, 400.00),
+(15, 3, 31, 'Kankernaam', 'approved', 2, 200.00),
+(16, 4, 45, 'liuyt', 'approved', 2, 300.00);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sections`
+-- Tábla szerkezet ehhez a táblához `sections`
 --
 
 DROP TABLE IF EXISTS `sections`;
@@ -159,11 +173,11 @@ CREATE TABLE IF NOT EXISTS `sections` (
 ) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `sections`
+-- A tábla adatainak kiíratása `sections`
 --
 
 INSERT INTO `sections` (`id`, `section_name`, `is_visible`) VALUES
-(1, 'about', 1),
+(1, 'about', 0),
 (2, 'services', 1),
 (3, 'gallery', 1),
 (4, 'banner-content', 1),
@@ -172,13 +186,13 @@ INSERT INTO `sections` (`id`, `section_name`, `is_visible`) VALUES
 (7, 'sneaker', 1),
 (8, 'exclusive', 1),
 (9, 'podium', 1),
-(10, 'stand-map-section', 1),
+(10, 'stand-map-section', 0),
 (11, 'pricing', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stands`
+-- Tábla szerkezet ehhez a táblához `stands`
 --
 
 DROP TABLE IF EXISTS `stands`;
@@ -187,80 +201,104 @@ CREATE TABLE IF NOT EXISTS `stands` (
   `plain_id` int DEFAULT NULL,
   `stand_number` varchar(50) NOT NULL,
   `is_available` tinyint(1) DEFAULT '1',
+  `price_per_day` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `plain_id` (`plain_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `stands`
+-- A tábla adatainak kiíratása `stands`
 --
 
-INSERT INTO `stands` (`id`, `plain_id`, `stand_number`, `is_available`) VALUES
-(1, 1, 'A', 0),
-(2, 1, 'AA', 1),
-(3, 1, 'A', 1),
-(4, 1, 'AA+', 1),
-(5, 1, 'A', 1),
-(6, 1, 'AA+', 1),
-(7, 1, 'AA', 1),
-(8, 1, 'A', 1),
-(9, 1, 'A', 1),
-(10, 1, 'AA', 1),
-(11, 1, 'AA+', 1),
-(12, 1, 'A', 1),
-(13, 1, 'AA', 1),
-(14, 1, 'A', 1),
-(15, 1, 'AA+', 1),
-(16, 2, 'A', 1),
-(17, 2, 'AA+', 1),
-(18, 2, 'AA', 1),
-(19, 2, 'A', 1),
-(20, 2, 'AA', 1),
-(21, 2, 'AA+', 1),
-(22, 2, 'A', 1),
-(23, 2, 'A', 1),
-(24, 2, 'AA', 1),
-(25, 2, 'AA+', 1),
-(26, 2, 'A', 1),
-(27, 2, 'A', 1),
-(28, 2, 'AA', 1),
-(29, 2, 'A', 1),
-(30, 2, 'AA+', 1),
-(31, 3, 'A', 1),
-(32, 3, 'AA+', 1),
-(33, 3, 'AA', 1),
-(34, 3, 'A', 1),
-(35, 3, 'AA', 1),
-(36, 3, 'AA+', 1),
-(37, 3, 'A', 1),
-(38, 3, 'AA', 1),
-(39, 3, 'AA+', 1),
-(40, 3, 'A', 1),
-(41, 3, 'AA', 1),
-(42, 3, 'A', 1),
-(43, 3, 'A', 1),
-(44, 3, 'AA+', 1),
-(45, 4, 'AA', 1),
-(46, 4, 'A', 1),
-(47, 4, 'AA+', 1),
-(48, 4, 'A', 1),
-(49, 4, 'AA', 1),
-(50, 4, 'A', 1),
-(51, 4, 'AA+', 1),
-(52, 4, 'A', 1),
-(53, 4, 'AA', 1),
-(54, 4, 'AA+', 1),
-(55, 4, 'A', 1),
-(56, 4, 'AA', 1),
-(57, 4, 'AA+', 1),
-(58, 5, 'A', 1),
-(59, 5, 'AA+', 1),
-(60, 5, 'AA', 1);
+INSERT INTO `stands` (`id`, `plain_id`, `stand_number`, `is_available`, `price_per_day`) VALUES
+(1, 1, 'A', 1, 100.00),
+(2, 1, 'AA', 0, 150.00),
+(3, 1, 'A', 1, 100.00),
+(4, 1, 'AA+', 1, 200.00),
+(5, 1, 'A', 1, 100.00),
+(6, 1, 'AA+', 1, 200.00),
+(7, 1, 'AA', 1, 150.00),
+(8, 1, 'A', 1, 100.00),
+(9, 1, 'A', 1, 100.00),
+(10, 1, 'AA', 1, 150.00),
+(11, 1, 'AA+', 1, 200.00),
+(12, 1, 'A', 1, 100.00),
+(13, 1, 'AA', 1, 150.00),
+(14, 1, 'A', 1, 100.00),
+(15, 1, 'AA+', 1, 200.00),
+(16, 2, 'A', 1, 100.00),
+(17, 2, 'AA+', 1, 200.00),
+(18, 2, 'AA', 1, 150.00),
+(19, 2, 'A', 1, 100.00),
+(20, 2, 'AA', 1, 150.00),
+(21, 2, 'AA+', 1, 200.00),
+(22, 2, 'A', 1, 100.00),
+(23, 2, 'A', 1, 100.00),
+(24, 2, 'AA', 1, 150.00),
+(25, 2, 'AA+', 0, 200.00),
+(26, 2, 'A', 1, 100.00),
+(27, 2, 'A', 1, 100.00),
+(28, 2, 'AA', 1, 150.00),
+(29, 2, 'A', 1, 100.00),
+(30, 2, 'AA+', 1, 200.00),
+(31, 3, 'A', 0, 100.00),
+(32, 3, 'AA+', 1, 200.00),
+(33, 3, 'AA', 1, 150.00),
+(34, 3, 'A', 1, 100.00),
+(35, 3, 'AA', 1, 150.00),
+(36, 3, 'AA+', 1, 200.00),
+(37, 3, 'A', 1, 100.00),
+(38, 3, 'AA', 1, 150.00),
+(39, 3, 'AA+', 1, 200.00),
+(40, 3, 'A', 1, 100.00),
+(41, 3, 'AA', 1, 150.00),
+(42, 3, 'A', 1, 100.00),
+(43, 3, 'A', 1, 100.00),
+(44, 3, 'AA+', 1, 200.00),
+(45, 4, 'AA', 0, 150.00),
+(46, 4, 'A', 1, 100.00),
+(47, 4, 'AA+', 1, 200.00),
+(48, 4, 'A', 1, 100.00),
+(49, 4, 'AA', 1, 150.00),
+(50, 4, 'A', 1, 100.00),
+(51, 4, 'AA+', 1, 200.00),
+(52, 4, 'A', 1, 100.00),
+(53, 4, 'AA', 1, 150.00),
+(54, 4, 'AA+', 1, 200.00),
+(55, 4, 'A', 1, 100.00),
+(56, 4, 'AA', 1, 150.00),
+(57, 4, 'AA+', 1, 200.00),
+(58, 5, 'A', 1, 100.00),
+(59, 5, 'AA+', 1, 200.00),
+(60, 5, 'AA', 1, 150.00);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tábla szerkezet ehhez a táblához `stand_pricing`
+--
+
+DROP TABLE IF EXISTS `stand_pricing`;
+CREATE TABLE IF NOT EXISTS `stand_pricing` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `stand_type` varchar(50) NOT NULL,
+  `price_per_day` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- A tábla adatainak kiíratása `stand_pricing`
+--
+
+INSERT INTO `stand_pricing` (`id`, `stand_type`, `price_per_day`) VALUES
+(1, 'A', 100.00),
+(2, 'AA', 150.00),
+(3, 'AA+', 200.00);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -275,7 +313,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `user`
+-- A tábla adatainak kiíratása `user`
 --
 
 INSERT INTO `user` (`id`, `Username`, `Email`, `Password`, `PreOrder`, `date`) VALUES
@@ -304,7 +342,7 @@ INSERT INTO `user` (`id`, `Username`, `Email`, `Password`, `PreOrder`, `date`) V
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tábla szerkezet ehhez a táblához `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -331,7 +369,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users`
+-- A tábla adatainak kiíratása `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `country`, `street`, `adres`, `city`, `state_province`, `zip_postal_code`, `email`, `password`, `created_at`, `is_admin`, `about`, `profile_photo`, `cover_photo`) VALUES
@@ -339,7 +377,7 @@ INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `country`, `st
 (2, 'Jane Smith', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'jane@example.com', 'passwordhash2', '2024-10-24 14:04:16', 0, NULL, NULL, NULL),
 (3, 'NimrodLobozar', 'F. Nimród', '', NULL, NULL, NULL, NULL, NULL, NULL, 'nimrod.lobozar@gmail.com', '$2y$10$ghJvhk2QKyVAoBnwJ8M.cOSfJCTnv/RKhfS4XC5Wyp87eNNSz.sNO', '2024-10-24 14:04:29', 1, '', 'assets/img/uploads/profile_3_294052465_5222177057837600_119414460320895139_n.jpg', 'assets/img/default/default-cover.jpg'),
 (4, 'TestUser', 'F. Nimród', 'Lobozár', 'Nederland', 'Australie', '25', 'Utrecht', 'Utrecht', '6574PL', 'test@gmail.com', '$2y$10$Mz8DQZgpGzkjx0fyCFYj2O2.vibTbhYxxvk25FhmbZQxZ2lAZS86u', '2024-10-26 22:48:05', 0, '', 'assets/img/uploads/profile_6_imageedit_2_2174348917-300x300.png', 'assets/img/uploads/cover_4_ferenc.lobozar_Hungarian_style_castle__surrounded_by_pine_tree__1b7b63a0-7855-4855-b3ba-45d1b13326c3.png');
-
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
