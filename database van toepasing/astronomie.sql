9ec7d12d7415219226a2f4b3feb1be320aeb1921
NimrodLoozar <nimrod.lobozarl.com>
2024-11-06 00:25:13 +0100

    Update generation time in astronomy SQL file

--- database van toepasing/astronomie.sql
+++ database van toepasing/astronomie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 05, 2024 at 09:47 PM
+-- Generation Time: Nov 05, 2024 at 11:24 PM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
